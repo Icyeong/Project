851aedbdc0a871de2e89c5f23106e1b1541a2da8
Yeong <dbcjswjs@naver.com>
2024-05-23 23:05:35 +0900

    rename: 기본 폴더 구조 설정

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,10 @@
+/** @jsxImportSource @emotion/react */
+"use client";
+import { css } from "@emotion/react";
+
+const style = css`
+  color: red;
+`;
+export default function Home() {
+  return <div css={style}>hi</div>;
+}
