3194acbb2888cbbc2b952ae45e4f3f99b9214618
Yeong <dbcjswjs@gmail.com>
2024-05-27 20:41:40 +0900

    design: 공통 css 추가

---
+++ src/app/_styles/cardWrapper.style.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const CardWarpper = styled.div`
+  max-width: 350px;
+  width: 100%;
+  border: 1px solid var(--border-light-gray);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
