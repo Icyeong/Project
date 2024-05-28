dfe3d1d678a7f6aebd86edd7bc881001dcd678e0
Yeong <dbcjswjs@gmail.com>
2024-05-29 04:15:08 +0900

    refactor: label문구 분리

--- src/app/_components/organism/form/LoginForm.tsx
+++ src/app/_components/organism/form/LoginForm.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Form } from "./LoginForm.style";
 import LabeledInput from "../../molecules/Input/LabeledInput";
 import Button from "../../atoms/button/BlueButton";
+import { INPUT_TEXT } from "@/app/_constant/input";
+const { LOGINID, PASSWORD } = INPUT_TEXT;
 
 export default function LoginForm() {
   const [text, setText] = useState("");
@@ -22,8 +24,8 @@ export default function LoginForm() {
 
   return (
     <Form>
-      <LabeledInput label="전화번호, 사용자 이름 또는 이메일" type="text" value={text} onChange={handleTextChange} />
-      <LabeledInput label="비밀번호" type="password" value={password} onChange={handlePasswordChange} />
+      <LabeledInput label={LOGINID} type="text" value={text} onChange={handleTextChange} />
+      <LabeledInput label={PASSWORD} type="password" value={password} onChange={handlePasswordChange} />
       <Button value="로그인" onClick={handleLoginClick} isActive={isActive} />
       {/* <LabeledInput
         type="text"
---
+++ src/app/_constant/input.ts
@@ -0,0 +1,4 @@
+export const INPUT_TEXT = {
+  LOGINID: "전화번호, 사용자 이름 또는 이메일",
+  PASSWORD: "비밀번호",
+};
