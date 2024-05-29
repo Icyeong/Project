d047c7ace08fc0cb099082f0bad4bea6390c4b73
chaeyeong Imm <cyim@chonf.co.kr>
2024-05-29 17:15:07 +0900

    feat: cdn domain env추가

--- src/app/_components/atoms/common/Logo.style.tsx
+++ src/app/_components/atoms/common/Logo.style.tsx
@@ -1,8 +1,9 @@
+import { CDN_DOMAIN } from "@/app/env/env";
 import styled from "styled-components";
 
 export const LogoBox = styled.div`
   i {
-    background-image: url(https://static.cdninstagram.com/rsrc.php/v3/yM/r/8n91YnfPq0s.png);
+    background-image: url(${CDN_DOMAIN}/rsrc.php/v3/yM/r/8n91YnfPq0s.png);
     background-position: 0px -52px;
     background-size: auto;
     width: 175px;
