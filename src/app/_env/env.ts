81621098dae1bb422e7f517308f9621f99ab0b1c
chaeyeong Imm <cyim@chonf.co.kr>
2024-05-30 18:34:50 +0900

    feat: env 폴더 경로 수정 및 auth 도메인 추가

---
+++ src/app/_env/env.ts
@@ -0,0 +1,2 @@
+export const CDN_DOMAIN = process.env.NEXT_PUBLIC_CDN_URL || "";
+export const AUTH_DOMAIN = process.env.NEXT_PUBLIC_AUTH_API_URL || "";
--- src/app/env/env.ts
+++
@@ -1,1 +0,0 @@
-export const CDN_DOMAIN = process.env.NEXT_PUBLIC_CDN_URL || "";
