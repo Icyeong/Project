63719bc7b88095f237d8d250aa1b91e1c2473294
chaeyeong Imm <cyim@chonf.co.kr>
2024-05-30 14:54:47 +0900

    refactor: 로그인폼 코드개선

--- src/app/_components/organism/form/LoginForm.tsx
+++ src/app/_components/organism/form/LoginForm.tsx
@@ -1,38 +1,57 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { Form } from "./LoginForm.style";
 import LabeledInput from "../../molecules/Input/LabeledInput";
 import { INPUT_TEXT } from "@/app/_constant/input";
 import BaseButton from "../../atoms/button/BaseButton";
-const { LOGINID, PASSWORD } = INPUT_TEXT;
+const { USERID, PASSWORD } = INPUT_TEXT;
 
 export default function LoginForm() {
-  const [text, setText] = useState("");
-  const [password, setPassword] = useState("");
   const [isActive, setIsActive] = useState(false);
+  const [loginForm, setLoginForm] = useState({
+    userId: "",
+    password: "",
+  });
 
-  const handleTextChange = (value: string | number) => {
-    setText(value as string);
-  };
+  const handLoginFormChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.value.trim();
 
-  const handlePasswordChange = (value: string | number) => {
-    setPassword(value as string);
+    if (name === "userId") {
+      setLoginForm({ ...loginForm, userId: value });
+    } else if (name === "password") {
+      setLoginForm({ ...loginForm, password: value });
+    }
   };
 
   const handleLoginClick = () => {
     alert("로그인");
   };
 
+  useEffect(() => {
+    if (loginForm.userId && loginForm.password) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  }, [loginForm]);
+
   return (
     <Form>
-      <LabeledInput label={LOGINID} type="text" value={text} onChange={handleTextChange} />
-      <LabeledInput label={PASSWORD} type="password" value={password} onChange={handlePasswordChange} />
-      <BaseButton value="로그인" onClick={handleLoginClick} isActive={isActive} blueButton={true} />
-      {/* <LabeledInput
-        type="text"
-        value={text}
-        inputProps={{ type: "text", value: text, onChange: handleTextChange }}
-        label="전화번호, 사용자 이름 또는 이메일"
-      /> */}
+      <LabeledInput label={USERID} type="text" value={loginForm.userId} name="userId" onChange={handLoginFormChange} />
+      <LabeledInput
+        label={PASSWORD}
+        type="password"
+        value={loginForm.password}
+        name="password"
+        onChange={handLoginFormChange}
+      />
+      <BaseButton
+        value="로그인"
+        onClick={handleLoginClick}
+        isActive={isActive}
+        blueButton={true}
+        disabled={!isActive}
+      />
     </Form>
   );
 }
