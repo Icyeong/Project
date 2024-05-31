27d0eb43ab332f86c2e6427d2f8936ce525d4f6b
chaeyeong Imm <cyim@chonf.co.kr>
2024-05-31 17:53:19 +0900

    chore: useLogin -> firebase 사용 로그인

--- src/app/_services/auth_service.ts
+++ src/app/_services/auth_service.ts
@@ -2,22 +2,22 @@ import { useQuery } from "@tanstack/react-query";
 import { AUTH_DOMAIN } from "../_env/env";
 const LOGIN = "LOGIN";
 
-const useLogin = (fetchData: { userId: string; password: string }) => {
-  return useQuery({
-    queryKey: [LOGIN],
-    queryFn: () => {
-      return fetch(`${AUTH_DOMAIN}/signin`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        // body: JSON.stringify(fetchData),
-        body: JSON.stringify({ memberId: "test123456", password: "test123456" }), //임시 테스트 가능 데이터
-        credentials: "include",
-      }).then((res) => res.json());
-    },
-    enabled: false,
-  });
+//현재 미사용
+const useLogin = (fetchData: { email: string; password: string }) => {
+  // return useQuery({
+  //   queryKey: [LOGIN],
+  //   queryFn: () => {
+  //     return fetch(`${AUTH_DOMAIN}/signin`, {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       // body: JSON.stringify(fetchData),
+  //       body: JSON.stringify({ memberId: "test123456", password: "test123456" }), //임시 테스트 가능 데이터
+  //       credentials: "include",
+  //     }).then((res) => res.json());
+  //   },
+  //   enabled: false,
+  // });
 };
-
 export const AuthService = {
   useLogin,
 };
