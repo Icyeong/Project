9c75fe9b4011a41f03c9086454ac651dc8340eb6
chaeyeong Imm <cyim@chonf.co.kr>
2024-05-31 17:36:59 +0900

    feat: firebase 설정 추가

--- src/app/_env/env.ts
+++ src/app/_env/env.ts
@@ -1,2 +1,3 @@
 export const CDN_DOMAIN = process.env.NEXT_PUBLIC_CDN_URL || "";
 export const AUTH_DOMAIN = process.env.NEXT_PUBLIC_AUTH_API_URL || "";
+export const FIREBASE_API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "";
