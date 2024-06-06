c180559c26566535c7c8587c249f89d0c9321a3b
Yeong <dbcjswjs@gmail.com>
2024-06-06 13:57:03 +0900

    feature: 에러 메시지 분리 및 타입 수정

--- src/app/_utils/errorHandler.ts
+++ src/app/_utils/errorHandler.ts
@@ -1,8 +1,13 @@
-export const authErrorHandler = (error: any): { message: string } => {
+import { LOGIN_TEXT } from "../_constant/errors";
+
+interface authFirebaseError extends Error {
+  code?: string;
+}
+
+export const authErrorHandler = (error: authFirebaseError): { message: string } => {
   console.log("error code : ", error.code);
 
-  if (error.code === "auth/invalid-credential") {
-    return { message: "이메일, 비밀번호를 다시 확인해주세요." };
-  }
-  return { message: "" };
+  if (error.code && error.code in LOGIN_TEXT) {
+    return { message: LOGIN_TEXT[error.code as keyof typeof LOGIN_TEXT] };
+  } else return { message: LOGIN_TEXT.UNKNOWN };
 };
