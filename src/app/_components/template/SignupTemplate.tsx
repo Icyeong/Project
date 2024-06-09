c4c14b46e30ba02ceac4029b219017b6bb1e5f24
Yeong <dbcjswjs@gmail.com>
2024-06-10 03:05:08 +0900

    rename: 폴더 이동

---
+++ src/app/_components/template/SignupTemplate.tsx
@@ -0,0 +1,11 @@
+import { PageWrapper } from "@/app/_styles/common.style";
+import React from "react";
+import SignupCard from "../organism/group/SingupCard";
+
+export default function SignupTemplate() {
+  return (
+    <PageWrapper>
+      <SignupCard />
+    </PageWrapper>
+  );
+}
