8057cde844f05ca685d104c08daf4898bc907b12
Yeong <dbcjswjs@gmail.com>
2024-06-10 03:35:09 +0900

    feature: 유저, 피드 더미데이터 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {},
+  images: {
+    domains: ["avatars.githubusercontent.com", "cloudflare-ipfs.com", "loremflickr.com"],
+  },
   compiler: {
     styledComponents: true,
   },
---
+++ src/app/_dummyData/userDummy.ts
@@ -0,0 +1,14 @@
+import { faker } from "@faker-js/faker";
+
+export const STORY_USERS = [
+  { username: "zero", img: faker.image.avatar() },
+  { username: "remonade", img: faker.image.avatar() },
+  { username: "cokeZero", img: faker.image.avatar() },
+  { username: "lovebara", img: faker.image.avatar() },
+  { username: "ausSydney", img: faker.image.avatar() },
+  { username: "mayo_lover", img: faker.image.avatar() },
+  { username: "rendering_", img: faker.image.avatar() },
+  { username: "buffer_buff", img: faker.image.avatar() },
+  { username: "cooking_story", img: faker.image.avatar() },
+  { username: "heya_music", img: faker.image.avatar() },
+];
