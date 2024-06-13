de4bb82e71c80c54c99198333a80c35139516a82
chaeyeong Imm <cyim@chonf.co.kr>
2024-06-13 17:20:46 +0900

    design: 레이아웃 css 수정

--- src/app/_components/organism/storyBar/StoryBar.style.tsx
+++ src/app/_components/organism/storyBar/StoryBar.style.tsx
@@ -4,6 +4,7 @@ export const StoryStyle = {
   Container: styled.div`
     display: flex;
     width: 100%;
+    max-width: 630px;
     overflow-x: scroll;
     padding: 8px 0;
     &::-webkit-scrollbar {
