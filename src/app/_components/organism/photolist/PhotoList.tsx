fe751f5c418cc96cd7efd3a533425a1607aa3545
chaeyeong Imm <cyim@chonf.co.kr>
2024-06-14 13:20:22 +0900

    chore

--- src/app/_components/organism/photolist/PhotoList.tsx
+++ src/app/_components/organism/photolist/PhotoList.tsx
@@ -4,38 +4,6 @@ import { List } from "./PhotoList.style";
 import PhotoSet from "../../molecules/photoSet/PhotoSet";
 
 export default function PhotoList() {
-  const PPHOTO_PIECES = [
-    {
-      id: 1,
-      likes: 132,
-      comments: 32,
-    },
-    {
-      id: 2,
-      likes: 23,
-      comments: 55,
-    },
-    {
-      id: 3,
-      likes: 234,
-      comments: 876,
-    },
-    {
-      id: 4,
-      likes: 53,
-      comments: 6,
-    },
-    {
-      id: 5,
-      likes: 565,
-      comments: 5,
-    },
-    {
-      id: 6,
-      likes: 1342,
-      comments: 322,
-    },
-  ];
   return (
     <List.Container>
       <PhotoSet />
