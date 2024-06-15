0a24166eea61f44e7a91c10ff26b8ed611128a32
Yeong <dbcjswjs@gmail.com>
2024-06-16 03:06:29 +0900

    feature: msw handler 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,8 @@
+import { createFeeds } from "@/app/_dummyData/feedDummy";
 import { http, HttpResponse } from "msw";
 
 export const handlers = [
-  http.get("/test", () => {
-    return HttpResponse.json({
-      id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
-      firstName: "cy",
-      lastName: "I",
-    });
+  http.get("/feeds", () => {
+    return HttpResponse.json(createFeeds(15));
   }),
 ];
