b564fcb7e4536d8083634f7298b85f319a000a35
chaeyeong Imm <cyim@chonf.co.kr>
2024-06-18 15:42:22 +0900

    feature: skeleton UI 스토리바 적용

--- src/app/_components/organism/storyBar/StoryBar.style.tsx
+++ src/app/_components/organism/storyBar/StoryBar.style.tsx
@@ -1,9 +1,10 @@
+import { SkeletonAni } from "@/app/_styles/common.style";
 import styled from "styled-components";
 
 export const StoryStyle = {
   Container: styled.div`
     display: flex;
-    width: 100%;
+    width: 100% !important;
     max-width: 630px;
     overflow-x: scroll;
     padding: 8px 0;
@@ -12,3 +13,9 @@ export const StoryStyle = {
     }
   `,
 };
+
+export const StorySkeleton = styled(StoryStyle.Container)`
+  background-color: #cccccc;
+  height: 127px;
+  animation: ${SkeletonAni} infinite alternate 1s;
+`;
--- src/app/_components/organism/storyBar/StoryBar.tsx
+++ src/app/_components/organism/storyBar/StoryBar.tsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { StoryStyle } from "./StoryBar.style";
+import { StorySkeleton, StoryStyle } from "./StoryBar.style";
 import User, { UserPorps } from "../../molecules/user/User";
 import { useCustomQuery } from "@/app/_hooks/useFetch";
 import { QUERY_KEYS } from "@/app/_stores/server/queryKeys";
 
 export default function StoryBar() {
-  const { data } = useCustomQuery(QUERY_KEYS.STORY.LIST.queryKey, QUERY_KEYS.STORY.LIST.queryFn);
+  const { data, isLoading } = useCustomQuery(QUERY_KEYS.STORY.LIST.queryKey, QUERY_KEYS.STORY.LIST.queryFn);
   return (
-    <StoryStyle.Container>
-      {data && data.map((user: UserPorps) => <User key={user.username} {...user} />)}
-    </StoryStyle.Container>
+    <>
+      {isLoading && <StorySkeleton />}
+      {data && (
+        <StoryStyle.Container>
+          {data.map((user: UserPorps) => (
+            <User key={user.username} {...user} />
+          ))}
+        </StoryStyle.Container>
+      )}
+    </>
   );
 }
