88b30b5bda9a693f74b3c07825b818d026d70509
chaeyeong Imm <cyim@chonf.co.kr>
2024-06-20 20:53:10 +0900

    remove: 불필요 파일, 코드 삭제

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,17 +1,7 @@
 "use client";
 import LoginPage from "@/app/_components/pages/LoginPage";
-import React, { useEffect } from "react";
+import React from "react";
 
 export default function page() {
-  useEffect(() => {
-    const mswFetch = async () => {
-      const res = await fetch("http://localhost:9090/test");
-      const data = await res.json();
-
-      console.log("msw fetch data : ", data);
-    };
-
-    mswFetch();
-  }, []);
   return <LoginPage />;
 }
