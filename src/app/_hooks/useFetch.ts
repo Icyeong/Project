2fe1de2b5efbb7da00369aa7dcb98d9104c08c3c
chaeyeong Imm <cyim@chonf.co.kr>
2024-06-21 17:17:16 +0900

    refactor: useCustomoMutation 훅 수정

--- src/app/_hooks/useFetch.ts
+++ src/app/_hooks/useFetch.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { MutationOptions, useMutation, useQuery } from "@tanstack/react-query";
 
 export const useCustomQuery = (
   key: readonly string[],
@@ -16,11 +16,20 @@ export const useCustomQuery = (
   });
 };
 
-export const useCustomMutation = (key: readonly string[], fn: any, onSuccess: any, onError: any) => {
-  return useMutation({
-    mutationKey: key,
-    mutationFn: fn,
-    onSuccess,
-    onError,
+interface CustomMutationOptions<TData, TError, TVariables, TContext = unknown>
+  extends Omit<MutationOptions<TData, TError, TVariables, TContext>, "mutationFn"> {
+  onSuccess?: (data: TData, variables: TVariables, context: TContext) => void;
+  onError?: (error: TError, variables: TVariables, context: TContext | undefined) => void;
+}
+
+export const useCustomMutation = <TData, TError, TVariables, TContext = unknown>(
+  mutationFn: (variables: TVariables) => Promise<TData>,
+  options?: CustomMutationOptions<TData, TError, TVariables, TContext>,
+) => {
+  return useMutation<TData, TError, TVariables, TContext>({
+    mutationFn,
+    onSuccess: options?.onSuccess,
+    onError: options?.onError,
+    ...options,
   });
 };
