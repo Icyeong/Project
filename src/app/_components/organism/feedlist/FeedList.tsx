23efe6ee5331643643ec58219189d3c31de41151
chaeyeong Imm <cyim@chonf.co.kr>
2024-07-01 16:51:45 +0900

    refactor: dependecy에 종속성 추가

--- src/app/_components/organism/feedlist/FeedList.tsx
+++ src/app/_components/organism/feedlist/FeedList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "./FeedList.style";
 import Feed, { FeedProps } from "@components/molecules/feed/Feed";
 import { useCustomQuery } from "@/_hooks/useFetch";
@@ -8,6 +8,7 @@ import FeedSkeleton from "./FeedSkeleton";
 import useFeedStore from "@/_stores/client/feedStore";
 
 export default function FeedList() {
+  const [isInitLoad, setIsInitLoad] = useState(true);
   const { data, isLoading } = useCustomQuery(QUERY_KEYS.FEED.LIST.queryKey, FeedService.getFeedsList, {
     gcTime: 1000 * 60 * 60,
     staleTime: 1000 * 60 * 60,
@@ -15,10 +16,11 @@ export default function FeedList() {
   const { setFeedsState, feedList } = useFeedStore();
 
   useEffect(() => {
-    if (data) {
+    if (data && isInitLoad) {
       setFeedsState(data);
+      setIsInitLoad(false);
     }
-  }, [data]);
+  }, [data, isInitLoad, setFeedsState]);
 
   return (
     <>
