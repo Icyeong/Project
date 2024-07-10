624af0c8e2bd0527e4cb92028fafa8bd5ef20361
chaeyeong Imm <cyim@chonf.co.kr>
2024-07-10 10:21:25 +0900

    refactor: constant파일 리터럴타입 객체로 수정

--- src/app/_constant/input.ts
+++ src/app/_constant/input.ts
@@ -3,4 +3,4 @@ export const INPUT_TEXT = {
   PASSWORD: "비밀번호",
   NAME: "성명",
   USERNAME: "사용자 이름",
-};
+} as const;
--- src/app/_constant/modal.ts
+++ src/app/_constant/modal.ts
@@ -1,7 +1,7 @@
 export const MODAL = {
   POST_FEED: "새 게시물 만들기",
   TEST: "TEST MODAL",
-};
+} as const;
 
 export type PostModalType = (typeof POST_MODAL)[keyof typeof POST_MODAL];
 export const POST_MODAL = {
