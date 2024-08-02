e4fe27162f7ab0a9cf92f7bea99bded5f8fb6a74
chaeyeong Imm <cyim@chonf.co.kr>
2024-08-02 16:24:36 +0900

    feature: 게시물 상세 페이지 notfound 추가

--- src/app/_components/organism/feedDetailBox/FeedDetailBox.tsx
+++ src/app/_components/organism/feedDetailBox/FeedDetailBox.tsx
@@ -5,11 +5,15 @@ import { usePathname } from "next/navigation";
 import { useCustomQuery } from "@/_hooks/useFetch";
 import { QUERY_KEYS } from "@/_stores/server/queryKeys";
 import { FeedService } from "@/_services/feed_service";
+import NotFound from "@/not-found";
 
 export default function FeedDetailBox() {
   const pathname = usePathname();
   const feedId = pathname.split("/")[2];
   const { data: feed } = useCustomQuery(QUERY_KEYS.FEED.DETAIL.queryKey, () => FeedService.getFeedDetail(feedId));
+
+  if (!feed) return <NotFound />;
+
   if (feed)
     return (
       <FeedDetail.Container>
