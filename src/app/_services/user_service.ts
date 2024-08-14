b836624ff776bec1861edf719a934bc574acc77c
chaeyeong Imm <cyim@chonf.co.kr>
2024-08-14 17:45:25 +0900

    feature: msw -> api방식으로 변경

--- src/app/_services/user_service.ts
+++ src/app/_services/user_service.ts
@@ -4,7 +4,7 @@ import { getErrorHandler, getFetchOptions } from "@/_utils/utils";
 
 const getFollowingList = async (userId: string) => {
   try {
-    const res = await fetch(`${BASE_DOMAIN}/following?userId=${userId}`);
+    const res = await fetch(`/api/following?userId=${userId}`);
     const data = await res.json();
     return data;
   } catch (error: unknown) {
@@ -14,7 +14,7 @@ const getFollowingList = async (userId: string) => {
 
 const getMyinfo = async () => {
   try {
-    const res = await fetch(`${BASE_DOMAIN}/myinfo`);
+    const res = await fetch(`/api/myinfo`);
     const data = await res.json();
     return data;
   } catch (error: unknown) {
@@ -24,7 +24,7 @@ const getMyinfo = async () => {
 
 const editMyinfo = async (fetchData: myinfoDetailProps) => {
   try {
-    const res = await fetch(`${BASE_DOMAIN}/myinfo`, getFetchOptions("PATCH", true, fetchData));
+    const res = await fetch(`/api/myinfo`, getFetchOptions("PATCH", true, fetchData));
     const data = await res.json();
     console.log("data : ", data);
     return data;
