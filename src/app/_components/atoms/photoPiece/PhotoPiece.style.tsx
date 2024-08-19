6fb6af72ff66cd761923f01e894426d0ec62387a
chaeyeong Imm <cyim@chonf.co.kr>
2024-08-19 14:16:46 +0900

    design: css수정

--- src/app/_components/atoms/photoPiece/PhotoPiece.style.tsx
+++ src/app/_components/atoms/photoPiece/PhotoPiece.style.tsx
@@ -18,15 +18,18 @@ export const Piece = {
     position: absolute;
     top: 0;
     left: 0;
-    background: rgba(0, 0, 0, 0.3);
+    background: rgba(0, 0, 0, 0.3) !important;
     display: none;
     justify-content: center;
     align-items: center;
+    div {
+      background: transparent !important;
+    }
   `,
 
   Count: styled.div`
     display: flex;
-    color: white;
+    color: white !important;
     font-size: 16px;
     font-weight: 600;
     padding: 0 17px;
